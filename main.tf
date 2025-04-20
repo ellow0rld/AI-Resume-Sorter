bcf93cc7b04a37613bdfe437beaf342d93ac1bbb
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-04-20 19:27:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ resource "aws_instance" "resume_app" {
   ami           = "ami-0db4215299a464a09"
   instance_type = "t2.micro"
   key_name      = "aws-terraform"
-  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   user_data = <<-EOF
               #!/bin/bash
@@ -14,7 +13,7 @@ resource "aws_instance" "resume_app" {
               amazon-linux-extras install docker -y
               service docker start
               usermod -a -G docker ec2-user
-              docker run -d -p 5000:5000 your-dockerhub-username/ai-resume-sorter
+              docker run -d -p 5000:5000 ellow0rld/ai-resume-sorter
               EOF
 
   tags = {
@@ -27,26 +26,29 @@ resource "aws_instance" "resume_app" {
 resource "aws_security_group" "resume_sg" {
   name        = "resume_sg"
   description = "Allow HTTP and SSH"
-  ingress = [
-    {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    },
-    {
-      from_port   = 5000
-      to_port     = 5000
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
-  egress = [
-    {
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  ]
+
+  ingress {
+    description = "Allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Flask App"
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
