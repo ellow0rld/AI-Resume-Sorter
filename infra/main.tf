facc4e7893535b2c9354b8093b70f4703095e986
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-05-01 17:11:34 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,39 +1,25 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "flask_app" {
   ami           = "ami-0db4215299a464a09"
   instance_type = "t2.micro"
-  key_name      = "aws-terraform"
 
-  user_data = file("deploy.sh")
+  key_name = var.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y python3 git
+              pip3 install flask
+              EOF
 
   tags = {
-    Name = "FlaskAppServer"
+    Name = "FlaskAppInstance"
   }
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
 }
 
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
-  description = "Allow SSH and HTTP"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 5000
-    to_port     = 5000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+output "public_ip" {
+  value = aws_instance.flask_app.public_ip
 }
