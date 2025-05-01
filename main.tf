2b39128950ffd62a0b9740874865e626e8224cc3
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-05-01 14:22:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,33 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "resume_app" {
+resource "aws_instance" "flask_app" {
   ami           = "ami-0db4215299a464a09"
   instance_type = "t2.micro"
   key_name      = "aws-terraform"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d -p 5000:5000 ellow0rld/ai-resume-sorter
-              EOF
+  user_data     = file("deploy.sh")
 
   tags = {
-    Name = "ResumeSorterApp"
+    Name = "FlaskAppServer"
   }
 
-  vpc_security_group_ids = [aws_security_group.resume_sg.id]
+  vpc_security_group_ids = [aws_security_group.allow_http.id]
 }
 
-resource "aws_security_group" "resume_sg" {
-  name        = "resume_sg"
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
   description = "Allow HTTP and SSH"
 
   ingress {
-    description = "Allow SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -36,7 +27,6 @@ resource "aws_security_group" "resume_sg" {
   }
 
   ingress {
-    description = "Allow Flask App"
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
@@ -44,11 +34,10 @@ resource "aws_security_group" "resume_sg" {
   }
 
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
+ 
