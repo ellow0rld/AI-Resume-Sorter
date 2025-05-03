885c122804f56ffcc813b71a16a2a5eaa1741399
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-05-04 00:46:45 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,20 +2,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_key_pair" "jenkins_key" {
-  key_name   = var.key_name
-  public_key = file(var.public_key_path)
-}
-
 resource "aws_security_group" "jenkins_sg" {
-  name        = "jenkins-sg"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  name        = "jenkins_sg"
+  description = "Security group for Jenkins and Flask app"
 
   ingress {
     from_port   = 8080
@@ -24,34 +13,52 @@ resource "aws_security_group" "jenkins_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "jenkins" {
-  ami                    = "ami-0db4215299a464a09" # Amazon Linux 2 (us-east-1)
+resource "aws_instance" "jenkins_instance" {
+  ami                    = "ami-0db4215299a464a09" 
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.jenkins_key.key_name
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install java-openjdk11 -y
-              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-              rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-              yum install jenkins -y
-              systemctl enable jenkins
-              systemctl start jenkins
-              EOF
+  key_name               = "aws-terraform"  
+  security_groups        = [aws_security_group.jenkins_sg.name]
+  user_data              = <<-EOF
+                            #!/bin/bash
+                            sudo yum update -y
+                            sudo yum install -y java-1.8.0-openjdk
+                            sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+                            sudo rpm --import https://pkg.jenkins.io/jenkins.io.key
+                            sudo yum install -y jenkins
+                            sudo systemctl start jenkins
+                            sudo systemctl enable jenkins
+                            EOF
 
   tags = {
-    Name = "Jenkins-Server"
+    Name = "Jenkins-Instance"
   }
+
+  # Associate public IP
+  associate_public_ip_address = true
 }
 
-  
+output "public_ip" {
+  value = aws_instance.jenkins_instance.public_ip
+}
