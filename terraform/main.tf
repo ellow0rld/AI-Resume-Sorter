f23d28af6a3e3a4119e21991f4df661deb23c6f7
ellow0rld <116413038+ellow0rld@users.noreply.github.com>
2025-05-06 17:14:42 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -30,33 +30,34 @@ resource "aws_security_group" "jenkins_sg" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_instance" "jenkins_instance" {
-  ami                    = "ami-0db4215299a464a09" 
+  ami                    = "ami-085386e29e44dacd7"
   instance_type          = "t2.micro"
-  key_name               = "aws-terraform"  
+  key_name               = "aws-terraform"
   security_groups        = [aws_security_group.jenkins_sg.name]
+  associate_public_ip_address = true
+
   user_data              = <<-EOF
                             #!/bin/bash
                             sudo yum update -y
-                            sudo yum install -y java-1.8.0-openjdk
-                            sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
-                            sudo rpm --import https://pkg.jenkins.io/jenkins.io.key
+                            sudo amazon-linux-extras enable java-openjdk11
+                            sudo yum install java-17-amazon-corretto -y
+                            sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+                            sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
                             sudo yum install -y jenkins
-                            sudo systemctl start jenkins
                             sudo systemctl enable jenkins
+                            sudo systemctl start jenkins
+                            sudo yum install -y git
                             EOF
 
   tags = {
     Name = "Jenkins-Instance"
   }
-
-  # Associate public IP
-  associate_public_ip_address = true
 }
 
 output "public_ip" {
