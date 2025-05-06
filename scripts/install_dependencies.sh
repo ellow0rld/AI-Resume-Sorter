72fc9c8deedaead90f92ab0fb9efeb57de297e61
Madhu Shree Aravindan <madhushreearavindan@gmail.com>
2025-05-06 17:32:38 +0530

    resolve conflict 2

--- scripts/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/ai-resume-sorter
-sudo yum install -y python3 python3-pip
-pip3 install --user -r requirements.txt
--- scripts/start_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/ai-resume-sorter
-nohup python3 app.py > output.log 2>&1 &
