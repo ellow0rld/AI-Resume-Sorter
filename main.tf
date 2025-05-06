31e1972f743a5ba04160924304bdef77acc615e9
Madhu Shree Aravindan <madhushreearavindan@gmail.com>
2025-05-06 17:16:01 +0530

    updates

--- main.tf
+++ main.tf
@@ -2,49 +2,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "resume_app" {
-  ami           = "ami-0db4215299a464a09"
-  instance_type = "t2.micro"
-  key_name      = "aws-terraform"
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              amazon-linux-extras install docker -y
-              service docker start
-              usermod -a -G docker ec2-user
-              docker run -d -p 5000:5000 ellow0rld/ai-resume-sorter
-              EOF
-
-  tags = {
-    Name = "ResumeSorterApp"
-  }
-
-  vpc_security_group_ids = [aws_security_group.resume_sg.id]
-}
-
-resource "aws_security_group" "resume_sg" {
-  name        = "resume_sg"
-  description = "Allow HTTP and SSH"
+resource "aws_security_group" "jenkins_sg" {
+  name        = "jenkins_sg"
+  description = "Security group for Jenkins and Flask app"
 
   ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    description = "Allow Flask App"
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
-    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -52,3 +35,31 @@ resource "aws_security_group" "resume_sg" {
   }
 }
 
+resource "aws_instance" "jenkins_instance" {
+  ami                    = "ami-085386e29e44dacd7"
+  instance_type          = "t2.micro"
+  key_name               = "aws-terraform"
+  security_groups        = [aws_security_group.jenkins_sg.name]
+  associate_public_ip_address = true
+
+  user_data              = <<-EOF
+                            #!/bin/bash
+                            sudo yum update -y
+                            sudo amazon-linux-extras enable java-openjdk11
+                            sudo yum install java-17-amazon-corretto -y
+                            sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+                            sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+                            sudo yum install -y jenkins
+                            sudo systemctl enable jenkins
+                            sudo systemctl start jenkins
+                            sudo yum install -y git
+                            EOF
+
+  tags = {
+    Name = "Jenkins-Instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.jenkins_instance.public_ip
+}
